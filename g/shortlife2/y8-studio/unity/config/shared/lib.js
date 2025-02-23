f4625ddfc01a174877a33b4fbc90454b51ba0415
James ⠀ <jdtwinterd@gmail.com>
2025-02-23 13:34:52 -0500

    adding short life 2

---
+++ g/shortlife2/y8-studio/unity/config/shared/lib.js
@@ -0,0 +1,10 @@
+!function() {
+    if (window.UnityLoader) {
+      if (window.UnityLoader.compatibilityCheck) {
+        window.UnityLoader.compatibilityCheck = function(element, callback, errCallback) {
+          callback();
+        }
+      }
+    }
+  }();
+  
