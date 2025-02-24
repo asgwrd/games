9d23831be51d8252c322a91b5877fc2e655222ac
James ⠀ <jdtwinterd@gmail.com>
2025-02-24 14:39:33 -0500

    adding slope

---
+++ g/slope/shared/gameBreakBeta.js
@@ -0,0 +1,20 @@
+window.adsbygoogle = window.adsbygoogle || [];
+const adBreak = adConfig = function(o) {adsbygoogle.push(o);}
+adConfig({
+    preloadAdBreaks: 'off',
+    sound: 'on', // This game has sound
+    onReady: () => {
+        console.log("ready");
+    }, // Called when API has initialised and adBreak() is ready
+});
+function showNextAd()
+{
+}
+
+function passBeforeAdData()
+{
+}
+
+function adBreakDoneData()
+{
+}
